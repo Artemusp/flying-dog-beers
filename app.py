482ca3d5e6c4e9a8622331a1649a48538d80b6f6
Artem <33843265+Artemusp@users.noreply.github.com>
2020-02-24 00:39:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,104 +5,75 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly
 from dash.dependencies import Input, Output
+import flask
+#import dash
+#import dash_html_components as html
+from flask import  jsonify,abort
+#from vkuser import audiofile
 
-# pip install pyorbital
-from pyorbital.orbital import Orbital
-satellite = Orbital('TERRA')
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.layout = html.Div(
-    html.Div([
-        html.H4('TERRA Satellite Live Feed'),
-        html.Button('Update time', id='button'),
-        dcc.Input(id='my-id', value='Text field', type='text'),
-        html.Div(id='my-div'),
-        html.Div(id='live-update-text'),
-        dcc.Graph(id='live-update-graph'),
-        dcc.Interval(
-            id='interval-component',
-            interval=1*1000, # in milliseconds
-            n_intervals=0
-        )
-    ])
-)
-@app.callback(
-    Output('my-div', 'children'),
-    [Input('button', 'n_clicks')],
-    )
-def update_output(n_clicks):
-    return 'Current time is {}'.format(
-        datetime.datetime.now().strftime("The time is: %H:%M:%S")
-    )
-# @app.callback(
-#     Output(component_id='my-div', component_property='children'),
-#     [Input(component_id='my-id', component_property='value')]
-# )
-# def update_output_div(input_value):
-#     return 'You\'ve entered "{}"'.format(input_value)
-
-@app.callback(Output('live-update-text', 'children'),
-              [Input('interval-component', 'n_intervals')])
-def update_metrics(n):
-    lon, lat, alt = satellite.get_lonlatalt(datetime.datetime.now())
-    style = {'padding': '5px', 'fontSize': '16px'}
-    return [
-        html.Span('Longitude: {0:.2f}'.format(lon), style=style),
-        html.Span('Latitude: {0:.2f}'.format(lat), style=style),
-        html.Span('Altitude: {0:0.2f}'.format(alt), style=style)
-    ]
+#user = audiofile.user
+app = flask.Flask(__name__)
+# RPS_DELAY = 0.34 # 1.5 ? audio.py vk_api
+@app.route('/')
+def index():
+    return 'Hello Flask app'
 
 
-# Multiple components can update everytime interval gets fired.
-@app.callback(Output('live-update-graph', 'figure'),
-              [Input('interval-component', 'n_intervals')])
-def update_graph_live(n):
-    satellite = Orbital('TERRA')
-    data = {
-        'time': [],
-        'Latitude': [],
-        'Longitude': [],
-        'Altitude': []
+# @server.route('/songs/<string:songName>', methods=['GET'])
+# def getSong(songName):
+#     songs = []
+#     #print("1")
+#     response = (user.searchSong(songName,10))
+#     #print(2)
+#     #print(response)
+#     #songs = response.first
+#     for j in response:
+#         #j["url"] = j["url"]#audiofile.codeSample(j["url"])
+#         #print(3)
+#         songs.append(j)
+#         # break
+#     #print(4)
+#     return jsonify(songs)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
     }
+]
 
-    # Collect some data
-    for i in range(180):
-        time = datetime.datetime.now() - datetime.timedelta(seconds=i*20)
-        lon, lat, alt = satellite.get_lonlatalt(
-            time
-        )
-        data['Longitude'].append(lon)
-        data['Latitude'].append(lat)
-        data['Altitude'].append(alt)
-        data['time'].append(time)
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
-    # Create the graph with subplots
-    fig = plotly.tools.make_subplots(rows=2, cols=1, vertical_spacing=0.2)
-    fig['layout']['margin'] = {
-        'l': 30, 'r': 10, 'b': 30, 't': 10
-    }
-    fig['layout']['legend'] = {'x': 0, 'y': 1, 'xanchor': 'left'}
 
-    fig.append_trace({
-        'x': data['time'],
-        'y': data['Altitude'],
-        'name': 'Altitude',
-        'mode': 'lines+markers',
-        'type': 'scatter'
-    }, 1, 1)
-    fig.append_trace({
-        'x': data['Longitude'],
-        'y': data['Latitude'],
-        'text': data['time'],
-        'name': 'Longitude vs Latitude',
-        'mode': 'lines+markers',
-        'type': 'scatter'
-    }, 2, 1)
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = False
+    for j in tasks:
+        if j["id"] == task_id:
 
-    return fig
-
+            task = j
+    print(task)
+    if not task:
+        abort(404)
+    return jsonify(task)
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run()
+
+
+
+
+# @server.route('/todo/api/v1.0/test')
+# def get_test():
+#     tests= Post.query.filter_by(author=current_user).all()
+#     return jsonify(tests)
